f70fd723391e48e4a8b6bd9391b249df89cc5c88
papa28x4 <swissfair@yahoo.co.uk>
2021-01-01 11:28:02 +0100

    seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,15 +13,82 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // User::truncate();
+        User::truncate();
         // factory(User::class, 8)->create();
-        User::create([
-            'name' => "Ekeoma",
-            'email' => "swissfair@yahoo.co.uk",
-            'password' => bcrypt('secret'),
-            'is_admin' => true,
-            'avatar' => '/images/users/9.jpg',
-            'department' => null
+    
+        DB::table('courses')->insert([
+            [
+                'name' => "Dannie Schoen",
+                'email' => "DannieS@example.net",
+                'password' => bcrypt('secret'),
+                'is_admin' => false,
+                'avatar' => '/images/users/6.jpg',
+                'department' => 'Accounting'
+            ],
+            [
+                'name' => "Orion Jacobs",
+                'email' => "OrionJ@example.net",
+                'password' => bcrypt('secret'),
+                'is_admin' => false,
+                'avatar' => '/images/users/1.jpg',
+                'department' => 'History'
+            ],
+            [
+                'name' => "Nadia McCulloug",
+                'email' => "NadiaM@example.org",
+                'password' => bcrypt('secret'),
+                'is_admin' => false,
+                'avatar' => '/images/users/5.jpg',
+                'department' => 'Computer Science'
+            ],
+            [
+                'name' => "Felipe VonRueden",
+                'email' => "FelipeV@example.com",
+                'password' => bcrypt('secret'),
+                'is_admin' => false,
+                'avatar' => '/images/users/2.jpg',
+                'department' => 'Electrical/Electronics'
+            ],
+            [
+                'name' => "Lempi Goodwin",
+                'email' => "LempiG@example.com",
+                'password' => bcrypt('secret'),
+                'is_admin' => false,
+                'avatar' => '/images/users/3.jpg',
+                'department' => 'Chemical Engineering'
+            ],
+            [
+                'name' => "Leone Jakubowski",
+                'email' => "LeoneJ@example.org",
+                'password' => bcrypt('secret'),
+                'is_admin' => false,
+                'avatar' => '/images/users/7.jpg',
+                'department' => 'Mathematics'
+            ],
+            [
+                'name' => "Stella Spencer",
+                'email' => "StellaS@example.org",
+                'password' => bcrypt('secret'),
+                'is_admin' => false,
+                'avatar' => '/images/users/8.jpg',
+                'department' => 'Electrical/Electronics'
+            ],
+            [
+                'name' => "Hudson Fahey",
+                'email' => "HudsonF@example.org",
+                'password' => bcrypt('secret'),
+                'is_admin' => false,
+                'avatar' => '/images/users/4.jpg',
+                'department' => 'Accounting'
+            ],
+            [
+                'name' => "Ekeoma",
+                'email' => "swissfair@yahoo.co.uk",
+                'password' => bcrypt('secret'),
+                'is_admin' => true,
+                'avatar' => '/images/users/9.jpg',
+                'department' => null
+            ]
         ]);
     }
 }
